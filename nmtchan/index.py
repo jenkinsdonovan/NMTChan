f8e789d25de5dc31e5f9fac577843319a60782a2
Donovan Jenkins <donovan.jenkins@student.nmt.edu>
2020-10-20 14:52:41 -0600

    show last git commits, and make greentexts less gay

--- nmtchan/index.py
+++ nmtchan/index.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, render_template, g
 from nmtchan import auth
 from nmtchan import db as database
+import git, time
 
 bp = Blueprint("index", __name__)
 @bp.route("/")
 @auth.require_login
 def handleIndex():
-    return render_template("index.html", level=g.user["level"])
+    repo = git.Repo(".")
+    commit = ""
+    if repo is not None:
+        commit = repo.head.commit
+    return render_template("index.html", level=g.user["level"], commit=commit)
 
 @bp.route("/rules/")
 def handleRules():
